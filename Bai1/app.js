8385dcd1c4feab763014cdf262169bc553623701
phuong <phuongdenpro@gmail.com>
2022-09-17 11:25:45 +0700

    read

--- Bai1/app.js
+++ Bai1/app.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const content = fs.readFileSync(__dirname + '/readme.txt', 'utf-8');
 console.log(content);
-fs.readFile(__dirname + '/readme.txt', function(err,data){
+fs.readFile(__dirname + '/readme.txt','utf-8', function(err,data){
     console.log(data);
+})
+
+var read = fs.createReadStream(__dirname + '/readme.txt');
+var wire = fs.createWriteStream(__dirname+'/readmecopy.txt');
+read.on('data', function(chunk){
+    console.log(chunk.length);
+    wire.write(chunk);
 })
