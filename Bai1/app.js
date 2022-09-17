dce6e9949a6f6a7f1c101840405a4993b9513b98
phuong <phuongdenpro@gmail.com>
2022-09-17 11:11:16 +0700

    a

--- Bai1/app.js
+++ Bai1/app.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 const content = fs.readFileSync(__dirname + '/readme.txt', 'utf-8');
-console.log(content)
+console.log(content);
+fs.readFile(__dirname + '/readme.txt', function(err,data){
+    console.log(data);
+})
