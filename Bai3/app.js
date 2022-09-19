1cf0691c8d9e542bd92ea03ac63385efc98713dd
phuong <phuongdenpro@gmail.com>
2022-09-19 19:49:05 +0700

    commit 2

--- Bai3/app.js
+++ Bai3/app.js
@@ -1,19 +1,21 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 
-//custom middleware
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/assets", express.static(__dirname + "/public"));
+// custom middleware
 app.use("/", function (request, response, next) {
   console.log("Request URL: ", request.url);
   request.requestTime = new Date();
   next();
 });
-app.use("/assets", express.static(__dirname + "/public"));
+
 app.get("/", function (request, response) {
-  response.send(`
-    <link href="/assets/style.css" rel="stylesheet" type="text.css">
-    <h1>Hello express</h1>
-    <p>Request time: ${request.requestTime}`);
+  response.render("index");
 });
 app.get("/api", function (request, response) {
   response.json({
@@ -22,7 +24,7 @@ app.get("/api", function (request, response) {
   });
 });
 app.get("/user/:id", function (request, response) {
-  response.send(`<h1>User: ${request.params.id}</h1>`);
+  response.render("user", { ID: request.params.id });
 });
 app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
