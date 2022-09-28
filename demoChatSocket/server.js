4f84508499e0939e6bc3675cd02e1ad2492e41bd
phuong <phuongdenpro@gmail.com>
2022-09-28 12:47:43 +0700

    demo chat

--- demoChatSocket/server.js
+++ demoChatSocket/server.js
@@ -9,14 +9,48 @@ app.use(express.static("./public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+var listUser = [];
 io.on("connection", (socket) => {
   console.log("Có người kết nối: " + socket.id);
 
+  socket.on("client-send-username", function (data) {
+    if (listUser.indexOf(data) >= 0) {
+      socket.emit("server-send-fail");
+    } else {
+      listUser.push(data);
+      socket.Username = data;
+      socket.emit("server-send-success", data);
+      io.sockets.emit("server-send-list-user", listUser);
+    }
+  });
 
-  
+  socket.on("logout", function () {
+    listUser.splice(listUser.indexOf(socket.Username), 1);
+    socket.broadcast.emit("server-send-list-user", listUser);
+  });
+  socket.on("user-send-message", function (data) {
+    io.sockets.emit("server-send-message", {
+      username: socket.Username,
+      mess: data,
+    });
+  });
+  socket.on("user-typing", function(){
+    var s = socket.Username + " is typing...";
+    io.sockets.emit("server-send-user-typing",s);
+  });
+  socket.on("user-stop-typing",function(){
+    io.sockets.emit("server-send-user-stop-typing");
+  });
+  socket.on("disconnect", () => {
+    console.log(socket.id+ " ngắt kết nối");
+    console.log(socket.Username+ " ngắt kết nối");
+    listUser.splice(listUser.indexOf(socket.Username), 1);
+    socket.broadcast.emit("server-send-list-user", listUser);
+  });
+
 });
 app.get("/", function (req, res) {
-  res.render("trangchu");
+  res.render("home");
 });
 
 server.listen(3000, function () {
